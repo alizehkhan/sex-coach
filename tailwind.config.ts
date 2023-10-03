6872e5dfd9753b6151543d2c35d5c917b4c91ad5
Alizeh Khan <alizehedinburgh@gmail.com>
2023-10-03 19:35:15 +0200

    feat: fonts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,6 +7,10 @@ const config: Config = {
     './app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
+    fontFamily: {
+      sans: ['Open Sans', 'sans-serif'],
+      serif: ['Petrona', 'serif'],
+    },
     extend: {
       backgroundImage: {
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
