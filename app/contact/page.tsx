9f526dd900f35ca9f9214423e3e11384629e8016
Alizeh Khan <alizehedinburgh@gmail.com>
2023-10-03 22:26:17 +0200

    chore: mdx

---
+++ app/contact/page.tsx
@@ -0,0 +1,9 @@
+const Contact = () => {
+  return (
+    <>
+      <div className="h-[calc(100vh-72px)] bg-[url('/haneen-2.png')] bg-cover bg-center bg-no-repeat"></div>
+    </>
+  )
+}
+
+export default Contact
