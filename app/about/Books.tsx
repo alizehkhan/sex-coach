ee1d05d7125b489550cb52709e06cf2fa0dfbcdb
Alizeh Khan <alizehedinburgh@gmail.com>
2023-10-04 09:50:34 +0200

    feat: subtle book ui

--- app/about/Books.tsx
+++ app/about/Books.tsx
@@ -8,20 +8,22 @@ const Books = () => {
       <h2 className="font-serif font-semibold text-4xl mt-40 text-stone-700 mb-8">
         Books I recommend
       </h2>
-      <div className="flex-1 flex flex-wrap gap-8">
+      <div className="grid gap-4 grid-cols-[repeat(auto-fit,minmax(320px,1fr))]">
         {BOOKS.map((book, i) => (
-          <div key={i} className="w-[200px]">
+          <div key={i} className="flex items-center gap-4">
             <Image
-              height={240}
-              width={200}
-              className="object-cover rounded-md w-full h-[264px]"
+              height={40}
+              width={64}
+              className="object-cover shrink-0 rounded-md w-10 h-16"
               src={book.image}
               alt=""
             />
-            <p className="text-lg mt-3 text-stone-700 font-semibold">
-              {book.title}
-            </p>
-            <p className="text-stone-500">{book.author}</p>
+            <div>
+              <p className="text-lg text-stone-700 font-semibold">
+                {book.title}
+              </p>
+              <p className="text-stone-500">{book.author}</p>
+            </div>
           </div>
         ))}
       </div>
