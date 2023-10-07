c6704ad723cab238910c32abfd2c2135f29f8f38
Alizeh Khan <alizehedinburgh@gmail.com>
2023-10-07 23:40:15 +0200

    feat: back button

--- app/blog/[slug]/page.tsx
+++ app/blog/[slug]/page.tsx
@@ -1,32 +1,30 @@
 import fs from 'fs'
 
+import { IconArrowLeft } from '@tabler/icons-react'
 import matter from 'gray-matter'
+import Link from 'next/link'
 import { MDXRemote } from 'next-mdx-remote/rsc'
-
-import getPostMetadata from '../getPostMetaData'
 
 import Container from '@/app/_components/Container'
 import { useMDXComponents } from '@/mdx-components'
 
 const getPostContent = (slug: string) => {
-  const folder = 'app/_posts/'
-  const file = `${folder}${slug}.mdx`
+  const file = `app/_posts/${slug}.mdx`
   const content = fs.readFileSync(file, 'utf8')
   return matter(content)
 }
 
-export const generateStaticParams = async () => {
-  const posts = getPostMetadata()
-  return posts.map((post) => ({
-    slug: post.slug,
-  }))
-}
-
 const PostPage = ({ params }: { params: { slug: string } }) => {
   const post = getPostContent(params.slug)
+
   return (
     <Container>
-      <h1 className="my-10 font-serif text-5xl font-semibold text-stone-700 tracking-tight md:text-7xl">
+      <Link href="/blog">
+        <div className="p-4 border-2 w-fit border-solid border-stone-700 rounded-full hover:bg-stone-200">
+          <IconArrowLeft />
+        </div>
+      </Link>
+      <h1 className="my-8 font-serif text-5xl font-semibold text-stone-700 tracking-tight md:text-7xl">
         {post.data.title}
       </h1>
       <article>
