24323e9b5521850b7cb82a249ec0db75c200d3a5
Alizeh Khan <alizehedinburgh@gmail.com>
2023-10-10 17:30:46 +0200

    feat: focus style

--- app/_components/CouplesCoaching.tsx
+++ app/_components/CouplesCoaching.tsx
@@ -15,13 +15,13 @@ const CouplesCoaching = () => {
         />
       </div>
       <div className="flex-1 text-lg">
-        <h2 className="md:text-6xl text-4xl text-stone-700 leading-tight font-serif tracking-tight font-semibold">
+        <h2 className="md:text-5xl text-4xl text-stone-700 leading-tight font-serif md:leading-tight tracking-tight font-semibold">
           Couples Intimacy Coaching
         </h2>
-        <p className="text-2xl font-semibold text-stone-700 leading-tight font-serif tracking-tight mt-4">
+        <p className="text-2xl font-semibold text-stone-500 leading-tight font-serif tracking-tight mt-4">
           An 8-week journey back to each other 🔥
         </p>
-        <p className="text-stone-700 mt-4">
+        <p className="text-stone-600 mt-4">
           I’ve coached hundreds of couples to break out of routine and feel more
           pleasure, desire, intimacy and aliveness in their sexual connection. I
           give couples a rich understanding of their needs & turn-ons, and make
@@ -29,7 +29,7 @@ const CouplesCoaching = () => {
         </p>
         <Link
           href="/coaching"
-          className="text-stone-600 mt-6 hover:gap-4 font-semibold transition-all items-center flex gap-1 underline underline-offset-8"
+          className="text-stone-700 w-fit mt-6 hover:gap-4 font-semibold transition-all items-center flex gap-1 underline underline-offset-8"
         >
           Learn more
           <IconChevronRight stroke={1.5} />
